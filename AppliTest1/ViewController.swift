07c3d1c8783051a6c3d0aa007d6af9c641ffceb5
Geert Vanhoorebeek <Geert@mbppleniortek2>
2016-11-18 21:01:23 +0100

    Première version

--- AppliTest1/ViewController.swift
+++ AppliTest1/ViewController.swift
@@ -10,14 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+
+    @IBOutlet weak var nomLabel: UILabel! = nil
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    @IBAction func nomAction(_ nomTextField: UITextField) {
+        nomLabel.text = "Salut \(nomTextField.text!)"
     }
 
 
